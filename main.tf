9088fbf5532b2016862973c0b25bf1b11ac89a6c
aniljoindevops <yerramaniianilkumar@gmail.com>
2024-05-30 09:06:54 +0530

    expense

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ resource "aws_security_group" "allow_tls" {
   name        = local.sg_name_final
   description = var.sg_description
   vpc_id      = var.vpc_id
-
- dynamic "ingress"  {
+  
+  dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-        from_port       = egress.value["from port"]
-        to_port         = egress.value["to_pprt"]
+        from_port       = egress.value["from_port"]
+        to_port         = egress.value["to_port"]
         protocol        = egress.value["protocol"]
         cidr_blocks = egress.value["cidr_blocks"]
     }
   }
 
-  dynamic "egress"  {
+  dynamic "egress" {
     for_each = var.outbound_rules
     content {
-        from_port       = egress.value["from port"]
-        to_port         = egress.value["to_pprt"]
+        from_port       = egress.value["from_port"]
+        to_port         = egress.value["to_port"]
         protocol        = egress.value["protocol"]
         cidr_blocks = egress.value["cidr_blocks"]
     }
